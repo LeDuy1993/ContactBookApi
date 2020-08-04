f3f916869c93e33120e3f4d0ba08ccc58df750c1
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-04 08:11:41 +0700

    IStudentRepoysity

--- ContactBook.DAL.Interface/IStudentRepository.cs
+++ ContactBook.DAL.Interface/IStudentRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using ContactBook.Domain.Responses.Student;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ContactBook.DAL.Interface
 {
      public interface IStudentRepository
     {
+        public Task<IEnumerable<GetStudentAll>> GetStudentAll();
+        public Task<GetStudentDetail> GetStudentDetail(int studentid);
+
     }
 }
