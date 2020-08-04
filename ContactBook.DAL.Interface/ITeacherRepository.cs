466737a706ac415adde00b243ce923a78cbccae9
tranhuuduc215 <tranhuuduc215@gmai.com>
2020-08-04 10:24:10 +0700

    CRUD Teacher

--- ContactBook.DAL.Interface/ITeacherRepository.cs
+++ ContactBook.DAL.Interface/ITeacherRepository.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using ContactBook.Domain.Repuests.Teacher;
+using ContactBook.Domain.Responses.Teacher;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ContactBook.DAL.Interface
 {
-     public interface ITeacherRepository
+    public interface ITeacherRepository
     {
+        Task<IEnumerable<TeacherView>> GetAllTeacher();
+        Task<TeacherView> GetTeacherById(int teacherId);
+        Task<DeleteTeacherResult> Delete(int teacherId);
+        Task<SaveTeacherResult> Save(SaveTeacherRequest request);
     }
 }
--- ContactBook.DAL/TeacherRepository.cs
+++ ContactBook.DAL/TeacherRepository.cs
@@ -1,6 +1,9 @@
 ﻿using ContactBook.DAL.Interface;
+using ContactBook.Domain.Repuests.Teacher;
 using ContactBook.Domain.Responses.Course;
+using ContactBook.Domain.Responses.Teacher;
 using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
@@ -9,6 +12,64 @@ namespace ContactBook.DAL
 {
     public class TeacherRepository : BaseRepository, ITeacherRepository
     {
-        
+        public async Task<IEnumerable<TeacherView>> GetAllTeacher()
+        {
+            return await SqlMapper.QueryAsync<TeacherView>(cnn: connection,
+                                          sql: "sp_Get_Teacher_All", 
+                                          commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<TeacherView> GetTeacherById(int teacherId)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@TeacherId", teacherId);
+            return (await SqlMapper.QueryFirstOrDefaultAsync<TeacherView>(cnn: connection,
+                            param: parameters,
+                            sql: "sp_Get_Teacher_ById",
+                            commandType: CommandType.StoredProcedure));
+        }
+
+        public async Task<SaveTeacherResult> Save(SaveTeacherRequest request)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@TeacherId", request.TeacherId);
+                parameters.Add("@TeacherName", request.TeacherName);
+                //parameters.Add("@DayOfBirth", DateTime.Parse(request.DayOfBirth));
+                parameters.Add("@DayOfBirth",request.DayOfBirth);
+                parameters.Add("@PlaceOfBirth", request.PlaceOfBirth);
+                parameters.Add("@Gender", bool.Parse(request.Gender));
+                parameters.Add("@Phone", request.Phone);
+                parameters.Add("@Address", request.Address);
+                parameters.Add("@SubjectId", request.SubjectId);
+                parameters.Add("@AvatarPath", request.AvatarPath);
+                parameters.Add("@DegreeId", request.DegreeId);
+                parameters.Add("@DayToWork", request.DayToWork);
+                return (await SqlMapper.QueryFirstOrDefaultAsync<SaveTeacherResult>(cnn: connection,
+                                            sql: "sp_Save_Teacher",
+                                            param: parameters,
+                                            commandType: CommandType.StoredProcedure));
+            }
+            catch (Exception ex)
+            {
+                return new SaveTeacherResult()
+                {
+                    TeacherId = 0,
+                    Message = "Something went wrong, please try again"
+                };
+            }
+
+        }
+
+        public async Task<DeleteTeacherResult> Delete(int teacherId)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@TeacherId", teacherId);
+            return await SqlMapper.QueryFirstOrDefaultAsync<DeleteTeacherResult>(cnn: connection,
+                             param: parameters,
+                            sql: "sp_Delete_Teacher",
+                            commandType: CommandType.StoredProcedure);
+        }
     }
 }
