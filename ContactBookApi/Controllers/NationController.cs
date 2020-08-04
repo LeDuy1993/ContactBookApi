7c94afb98fa237e8cf07eb6da6854c1036a068db
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2020-08-04 10:00:23 +0700

    make content for NationController description

--- ContactBookApi/Controllers/NationController.cs
+++ ContactBookApi/Controllers/NationController.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace ContactBookApi.Controllers
 {
     /// <summary>
-    /// 
+    /// Nation controller
     /// </summary>
     [ApiController]
     public class NationController : ControllerBase
