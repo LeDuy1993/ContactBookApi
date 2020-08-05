2236ff59b64c5936ad982bfab41f3cbea6b86522
tranhuuduc215 <tranhuuduc215@gmai.com>
2020-08-05 10:39:51 +0700

    Add Dgree and edit teacher

---
+++ ContactBook.Domain/Responses/Degree/DegreeView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactBook.Domain.Responses.Degree
+{
+   public class DegreeView
+    {
+        public int DegreeId { get; set; }
+        public string DegreeName { get; set; }
+    }
+}
