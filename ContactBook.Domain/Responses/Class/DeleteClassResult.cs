2fa229232dfe44c2df88efdb2bc8583de2e98693
an hong <hongan529@gmail.com>
2020-08-05 10:54:38 +0700

    save - delete Class, get class by GradeId - CourseId

---
+++ ContactBook.Domain/Responses/Class/DeleteClassResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactBook.Domain.Responses.Class
+{
+    public class DeleteClassResult
+    {
+        public int ClassId { get; set; }
+        public string Message { get; set; }
+    }
+}
