d394a93661a35fe9654cff532d2f6496859de8fd
an hong <hongan529@gmail.com>
2020-08-07 10:43:49 +0700

    fix add teacher

--- ContactBook.DAL/TeacherRepository.cs
+++ ContactBook.DAL/TeacherRepository.cs
@@ -45,16 +45,16 @@ namespace ContactBook.DAL
                 DynamicParameters parameters = new DynamicParameters();
                 parameters.Add("@TeacherId", request.TeacherId);
                 parameters.Add("@TeacherName", request.TeacherName);
-                //parameters.Add("@DayOfBirth", DateTime.Parse(request.DayOfBirth));
-                parameters.Add("@DayOfBirth",request.DayOfBirth);
+                parameters.Add("@DayOfBirth", request.DayOfBirth);
                 parameters.Add("@PlaceOfBirth", request.PlaceOfBirth);
-                parameters.Add("@Gender", bool.Parse(request.Gender));
+                parameters.Add("@Gender", request.Gender);
                 parameters.Add("@Phone", request.Phone);
                 parameters.Add("@Address", request.Address);
                 parameters.Add("@SubjectId", request.SubjectId);
                 parameters.Add("@AvatarPath", request.AvatarPath);
                 parameters.Add("@DegreeId", request.DegreeId);
                 parameters.Add("@DayToWork", request.DayToWork);
+                //parameters.Add("@DayToWork", request.DayToWork);
                 return (await SqlMapper.QueryFirstOrDefaultAsync<SaveTeacherResult>(cnn: connection,
                                             sql: "sp_Save_Teacher",
                                             param: parameters,
