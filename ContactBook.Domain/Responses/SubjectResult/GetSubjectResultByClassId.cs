a94617f083c336a12afafe21b703862d955003d8
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-14 23:19:07 +0700

    Duy_GetSubjectResultByClassId

--- ContactBook.Domain/Responses/SubjectResult/GetSubjectResultByClassId.cs
+++ ContactBook.Domain/Responses/SubjectResult/GetSubjectResultByClassId.cs
@@ -8,9 +8,10 @@ namespace ContactBook.Domain.Responses.SubjectResult
     {
         public int SubjectResultId { get; set; }
         public int StudentId { get; set; }
-        public int ClassId { get; set; }
-        public int SemesterId { get; set; }
         public int SubjectId { get; set; }
+        public int ClassStudentId { get; set; }
+        public int SemesterId { get; set; }
+        public int ClassSubjectId { get; set; }
         public string ListPoint { get; set; }
         public string ListDate { get; set; }
     }
