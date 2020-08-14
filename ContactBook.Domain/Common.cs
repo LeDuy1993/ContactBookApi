260f623780a50915b85a9980cd5269a916811562
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-14 10:15:39 +0700

    Duy_SQL_SeedDB and Schema

--- ContactBook.Domain/Common.cs
+++ ContactBook.Domain/Common.cs
@@ -6,6 +6,6 @@ namespace ContactBook.Domain
 {
    public class Common
     {
-        public static string ConnectionString = @"Data Source=desktop-b5sct1g\sqlexpress;Initial Catalog=WebContactBook;Integrated Security=True";
+        public static string ConnectionString = @"Data Source=duyle\sqlexpress;Initial Catalog=WebContactBook;Integrated Security=True";
     }
 }
--- ContactBook.Domain/Responses/SubjectResult/GetSubjectResultByClassId.cs
+++ ContactBook.Domain/Responses/SubjectResult/GetSubjectResultByClassId.cs
@@ -11,9 +11,7 @@ namespace ContactBook.Domain.Responses.SubjectResult
         public int ClassId { get; set; }
         public int SemesterId { get; set; }
         public int SubjectId { get; set; }
-        public string SubjectName { get; set; }
         public string ListPoint { get; set; }
         public string ListDate { get; set; }
-        public string TeacherName { get; set; }
     }
 }
