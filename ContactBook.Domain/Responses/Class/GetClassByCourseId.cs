8c83bb97160b9812d815d17f81e7f58ea4b683f5
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-16 13:04:19 +0700

    Duy_GetClassBhyCourseId_Added(Model)

---
+++ ContactBook.Domain/Responses/Class/GetClassByCourseId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactBook.Domain.Responses.Class
+{
+    public class GetClassByCourseId
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public int GradeId { get; set; }
+        public int CourseId { get; set; }
+        public int TeacherId { get; set; }
+        public int Students { get; set; }
+        public string TeacherName { get; set; }
+    }
+}
